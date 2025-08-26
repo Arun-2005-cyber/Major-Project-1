42401ef5da3531b7f8a05363668fd9b9c6048939
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-08-26 09:33:36 +0530

    new big commit

---
+++ backend/ecommerce/project/project/urls.py
@@ -0,0 +1,15 @@
+# project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.urls import path, include   # <-- path comes from here
+from django.conf import settings        # <-- import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('app.urls')),  # routes to your app
+]
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
