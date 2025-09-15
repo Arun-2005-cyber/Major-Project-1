324f92b6545bf8766e92547d39b63b31de8973b8
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-09-15 09:24:52 +0530

    new

--- backend/ecommerce/project/settings.py
+++ backend/ecommerce/project/settings.py
@@ -149,11 +149,12 @@ USE_TZ = True
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv("EMAIL_HOST", "smtpout.secureserver.net")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtpout.secureserver.net")  # Replace with your email host
+EMAIL_PORT = 587  # Common SMTP port for TLS
+EMAIL_USE_TLS = True  # Use TLS encryption
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "prakashsm940@gmail.com")  # Your email address
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "prakashsekar")  # Your email password
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
