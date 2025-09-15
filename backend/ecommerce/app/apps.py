7cd29899e9e04f804b219fa3cbdd9445569b73f4
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-09-15 12:42:36 +0530

    new

--- backend/ecommerce/app/apps.py
+++ backend/ecommerce/app/apps.py
@@ -6,16 +6,16 @@ class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = "app"
 
-    def ready(self):
-        # Auto-create superuser on deploy
-        User = get_user_model()
-        username = "prakashsm940@gmail.com"
-        email = "prakashsm940@gmail.com"
-        password = "prakashsekar"   # 👉 change this to a strong password
+    # def ready(self):
+    #     # Auto-create superuser on deploy
+    #     User = get_user_model()
+    #     username = "prakashsm940@gmail.com"
+    #     email = "prakashsm940@gmail.com"
+    #     password = "prakashsekar"   # 👉 change this to a strong password
 
-        if not User.objects.filter(username=username).exists():
-            try:
-                User.objects.create_superuser(username=username, email=email, password=password)
-                logging.info("Superuser created: %s", username)
-            except Exception as e:
-                logging.error("Could not create superuser: %s", e)
+    #     if not User.objects.filter(username=username).exists():
+    #         try:
+    #             User.objects.create_superuser(username=username, email=email, password=password)
+    #             logging.info("Superuser created: %s", username)
+    #         except Exception as e:
+    #             logging.error("Could not create superuser: %s", e)
