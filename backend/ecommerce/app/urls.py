13525f71d6a8844b63db6c16298f98c9c01d1d1a
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-10-21 10:07:18 +0530

    new

--- backend/ecommerce/app/urls.py
+++ backend/ecommerce/app/urls.py
@@ -8,35 +8,33 @@ from rest_framework_simplejwt.views import ( # type: ignore
 
 
 
-urlpatterns=[
-    path('',views.getRoutes,name="getRoutes"),
-    path('products/',views.getProducts,name="getProducts"),
-    path('product/<str:pk>',views.getProduct,name="getProduct"),
+urlpatterns = [
+    path('', views.getRoutes, name="getRoutes"),
+
+    # Product APIs
+    path('products/', views.getProducts, name="getProducts"),
+    path('product/<str:pk>/', views.getProduct, name="getProduct"),
+    path('products/create/', views.createProduct, name='product-create'),
+    path('products/update/<str:pk>/', views.updateProduct, name='product-update'),
+    path('products/delete/<str:pk>/', views.deleteProduct, name='product-delete'),
+    path('products/upload/', views.uploadImage, name='image-upload'),
+
+    # Auth APIs
     path('users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/register/',views.registerUser,name='register'),
-    path('activate/<uidb64>/<token>/',views.ActivateAccountView.as_view(),name='activate'),
-    path('orders/add/',views.addOrderItems,name='order_add'),
-    path('orders/',views.getOrders,name='orders'),
-    path('orders/myorders/',views.getMyOrders,name='myorders'),
-    path('orders/<str:pk>/',views.getOrderById,name='user-order'),
-    
-    # Admin URl
+    path('users/register/', views.registerUser, name='register'),
+    path('users/activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name='activate'),
 
-    path('products/create/',views.createProduct,name='product-create'),
-    path('products/update/<str:pk>/',views.updateProduct,name='product-update'),
-    path('products/delete/<str:pk>/',views.deleteProduct,name='product-delete'),
-    path('products/upload/',views.uploadImage,name='image-upload'),
-   
-   
-   
-    path('users/getallusers/',views.getUsers,name='users'),
-    path('users/update/<str:pk>/',views.updateUser,name='updateUser'),
-    path('users/delete/<str:pk>/',views.deleteUser,name='deleteUser'),
-    path('users/<int:pk>/',views.getUserById,name='getUserById'),
+    # User APIs
+    path('users/getallusers/', views.getUsers, name='users'),
+    path('users/update/<str:pk>/', views.updateUser, name='updateUser'),
+    path('users/delete/<str:pk>/', views.deleteUser, name='deleteUser'),
+    path('users/<int:pk>/', views.getUserById, name='getUserById'),
+    path('users/profile/', views.getUserProfile, name='getUserProfile'),
+    path('users/profile/update/', views.updateUserProfile, name='updateUserProfile'),
 
-    path('users/profile/',views.getUserProfile,name='getUserProfile'),
-    path('users/profile/update/',views.updateUserProfile,name='updateUserProfile')
-
-
-  
+    # Orders APIs
+    path('orders/add/', views.addOrderItems, name='order_add'),
+    path('orders/', views.getOrders, name='orders'),
+    path('orders/myorders/', views.getMyOrders, name='myorders'),
+    path('orders/<str:pk>/', views.getOrderById, name='user-order'),
 ]
--- backend/ecommerce/app/views.py
+++ backend/ecommerce/app/views.py
@@ -84,7 +84,7 @@ def registerUser(request):
             username=data['email'],
             email=data['email'],
             password=make_password(data['password']),
-            is_active=True
+            is_active=False
         )
 
         # Generate uid + token
