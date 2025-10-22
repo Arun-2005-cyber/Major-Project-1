395a705ecda68ac0c043a46e11bc184351b7406c
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-10-22 11:40:03 +0530

    new

--- backend/ecommerce/project/settings.py
+++ backend/ecommerce/project/settings.py
@@ -155,16 +155,17 @@ USE_TZ = True
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 BACKEND_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
 
-# Email
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+
+# 📧 Email Configuration (Brevo / Sendinblue)
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp-relay.brevo.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-        # paste 16-char app password
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-  # Your email password
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
+
 
 
 # Default primary key field type
