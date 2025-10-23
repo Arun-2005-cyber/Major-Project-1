df8be73542729a32647db01ce3d1ff7b775661a6
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-10-23 14:32:17 +0530

    NEW

--- backend/ecommerce/app/views.py
+++ backend/ecommerce/app/views.py
@@ -122,8 +122,8 @@ def registerUser(request):
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         token = default_token_generator.make_token(user)
 
-        # Build activation link using FRONTEND_URL from settings
-        activation_link = f"{settings.FRONTEND_URL.rstrip('/')}/activate/{uid}/{token}/"
+        # Build activation link using BACKEND_URL from settings
+        activation_link = f"{settings.BACKEND_URL.rstrip('/')}/activate/{uid}/{token}/"
         print("ACTIVATION LINK:", activation_link)
         # Send activation email
         email_subject = "Activate Your Account"
